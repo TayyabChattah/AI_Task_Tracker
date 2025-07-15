eefc246200fef78a54e44e6608f763e830e92273
Tayyab Chattah <111242774+TayyabChattah@users.noreply.github.com>
2025-07-15 18:29:21 +0500

    Update settings.py

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -80,7 +80,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(
-        'postgresql://postgres.nedifesaljgmewogvhil:Chattah00@7@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres'
+        ''
     )
 }
 
@@ -126,6 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # .env
-GEMINI_API_KEY="AIzaSyAikGbqL57_N4gMp1W_q4I9WwhjhGuKPNc"
+GEMINI_API_KEY=""
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ALLOWED_HOSTS = ['*']
